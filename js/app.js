68a07b199d1a7066935b01c1c32c760753fb21e1
Bogac Kerem Goksel <bkgoksel@gmail.com>
2016-03-06 09:49:11 -0800

    Connected to display candidates

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
 var candidateApp = angular.module('candidateApp', ['candidateControllers']);
-
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);

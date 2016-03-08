f7b4770c4b8b5741393ef3befe8683d0d15bc231
Bogac Kerem Goksel <bkgoksel@gmail.com>
2016-03-08 07:49:59 -0800

    added check to filter charte from homepage

--- js/controllers.js
+++ js/controllers.js
@@ -8,6 +8,7 @@ candidateApp.controller('candidateListCtrl', function($scope, $http) {
   $http.get('http://lvp.mod.bz/api/candidate/all', {
       withCredentials: true
   }).then(function(response) {
+      response.data.filter(function(cand) { return cand.firstName !== 'Charte'; });
       $scope.candidates = response.data;
       console.log('candidates: %j', response.data);
   }, function(response) {
